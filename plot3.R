10f1aaad122e2956a80cf8df5e432b6b13cb4ccd
Spock <spockpv@gmail.com>
2020-09-22 23:24:15 +0300

    Third plot

---
+++ plot3.R
@@ -0,0 +1,12 @@
+Sys.setlocale("LC_ALL","en_US.UTF-8")
+data<-read.csv("household_power_consumption.txt", sep = ";")
+data<-subset(data, Date=="2/2/2007" | Date=="1/2/2007")
+data<-cbind(data, as.POSIXct(paste(data$Date, data$Time), format="%d/%m/%Y %H:%M:%S"))
+colnames(data)[10]<-"DateTime"
+png(filename = "plot3.png")
+plot(data$DateTime, data$Sub_metering_1, type = "n", xlab = "", ylab = "Energy sub metering")
+points(data$DateTime, data$Sub_metering_1, type = "l", col="black")
+points(data$DateTime, data$Sub_metering_2, type = "l", col="red")
+points(data$DateTime, data$Sub_metering_3, type = "l", col="blue")
+legend("topright",legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = 1, col=c("black", "red", "blue"))
+dev.off()
